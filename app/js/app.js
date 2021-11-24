9bad13261abb50a96a0638f3c1f1b0c710e3a529
diana.dyakovskaya <diana.dyakovskaya7@gmail.com>
2021-11-24 16:56:15 +0300

    add submit a form

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
-  // $('body').hide()
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('#form')
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    let formData = new FormData(form)
+    await fetch('https://private-c4cefc-codepenvf.apiary-mock.com/sendform', {
+      method: 'POST',
+      body: formData
+    })
+      .then(response => response.json().then(data => alert(data.message)))
+      .catch(data => console.error(data))
+  })
+
 })
